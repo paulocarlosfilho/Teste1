fd9f90850e04eb09d7b6058da6ca7951ab969fa7
Paulo Carlos <paulocarlosfilho@gmail.com>
2025-02-20 13:41:17 -0300

    Feat: Projeto do App to_do finalizado. É possivel salvar, editar e excluir um item na lista.

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from to_do.views import to_do_list
+from to_do.views import To_do_List_View, To_Do_Created_View, to_do_update_view, to_do_delete_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', to_do_list)
+    path('', To_do_List_View.as_view(), name="to_do_list"),
+    path('creation', To_Do_Created_View.as_view(), name="to_do_creation"),
+    path('update/<int:pk>', to_do_update_view.as_view(), name="to_do_update"),
+    path('delete/<int:pk>', to_do_delete_view.as_view(), name="to_do_delete"),
 ]
--- to_do/models.py
+++ to_do/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class To_do(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Título')
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(null=False, blank=False)
+    deadline = models.DateField(null=False, blank=False, verbose_name='Data Final')
     fineshed_at = models.DateField(null=True, blank=False)
     observacoes = models.CharField( max_length=500, null=True, blank=False)
 
