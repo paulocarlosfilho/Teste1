8f11a7d684306eea7d4a7097dd9465fb58c3fd01
Paulo Carlos <paulocarlosfilho@gmail.com>
2025-02-24 11:51:21 -0300

    Feat: Botão Finalizar concluido - Primeira parte do projeto pronta

--- to_do/models.py
+++ to_do/models.py
@@ -7,6 +7,6 @@ class To_do(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Título')
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(null=False, blank=False, verbose_name='Data Final')
-    fineshed_at = models.DateField(null=True, blank=False)
+    finished_at = models.DateField(null=True, blank=True)
     observacoes = models.CharField( max_length=500, null=True, blank=False)
 
